af3b94e156e7fe5727be7163ab6447b224a10a09
Tien Nguyen <nntien.nguyen@gmail.com>
2018-07-05 22:43:14 +0700

    moble code

--- tinyproject/App.js
+++ tinyproject/App.js
@@ -4,19 +4,24 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Alert,
+  TouchableOpacity
+} from "react-native";
+
+const FBSDK = require("react-native-fbsdk");
+const { LoginButton, AccessToken, LoginManager } = FBSDK;
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
@@ -24,15 +29,88 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text> */}
+        {/* <LoginButton
+          publishPermissions={["public_profile"]}
+          onLoginFinished={(error, result) => {
+            if (error) {
+              Alert.alert(
+                "Alert Title",
+                result.error,
+                [
+                  {
+                    text: "Ask me later"
+                  },
+                  {
+                    text: "Cancel",
+                    style: "cancel"
+                  }
+                ],
+                { cancelable: false }
+              );
+            } else if (result.isCancelled) {
+              Alert.alert(
+                "Alert Title",
+                "login is cancelled.",
+                [
+                  {
+                    text: "Ask me later"
+                  },
+                  {
+                    text: "Cancel",
+                    style: "cancel"
+                  }
+                ],
+                { cancelable: false }
+              );
+            } else {
+              AccessToken.getCurrentAccessToken().then(data => {
+                Alert.alert(
+                  "Alert Title",
+                  data.accessToken.toString(),
+                  [
+                    {
+                      text: "Ask me later"
+                    },
+                    {
+                      text: "Cancel",
+                      style: "cancel"
+                    }
+                  ],
+                  { cancelable: false }
+                );
+              });
+            }
+          }}
+          onLogoutFinished={() => alert("logout.")}
+        /> */}
+        <TouchableOpacity
+          onPress={() => {
+            console.log("Login start");
+            LoginManager.logInWithReadPermissions(["public_profile"]).then(
+              function(result) {
+                if (result.isCancelled) {
+                  //alert("Login cancelled");
+                  console.log("Login cancelled");
+                } else {
+                  console.log(result.grantedPermissions.toString());
+                  // alert(
+                  //   "Login success with permissions: " +
+                  //     result.grantedPermissions.toString()
+                  // );
+                }
+              },
+              function(error) {
+                console.log("Login fail with error: " + error);
+                //alert("Login fail with error: " + error);
+              }
+            );
+          }}
+        >
+          <Text>Sign In</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -41,18 +119,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
